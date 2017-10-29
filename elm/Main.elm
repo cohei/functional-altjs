f5ecbdfb3e83726bf6af22ac62f9eb6dcb46d06f
TANIGUCHI Kohei <a.d.xvii.kal.mai@gmail.com>
2017-10-30 05:17:36 +0900

    Elm: add counter

--- elm/Main.elm
+++ elm/Main.elm
@@ -1,8 +1,39 @@
 module Main exposing (main)
 
-import Html exposing (Html, h1, div, text)
+import Html exposing (Html, h1, div, button, text, hr)
+import Html.Events exposing (onClick)
 
-main = Html.beginnerProgram { model = 0, view = view, update = \_ _ -> 0 }
 
-view : a -> Html msg
-view _ = div [] [h1 [] [text "Hello World!"]]
+main : Program Never Model Msg
+main =
+    Html.beginnerProgram { model = 0, view = view, update = update }
+
+
+type alias Model =
+    Int
+
+
+type Msg
+    = Decrement
+    | Increment
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Hello World!" ]
+        , hr [] []
+        , button [ onClick Decrement ] [ text "-" ]
+        , text (toString model)
+        , button [ onClick Increment ] [ text "+" ]
+        ]
